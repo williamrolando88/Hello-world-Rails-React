b1e0087a6c3cb854cca93e1884603ec10927892f
williamrolando88 <william.rolando88@hotmail.com>
2022-05-02 18:40:42 -0500

    Add: React-router to manage browsing

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Route, Router, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Greet from './Greet';
 
 const App = () => {
-  return <>App</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Greet />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
