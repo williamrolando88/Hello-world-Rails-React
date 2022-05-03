3f28f935a25ff2f59f0fff985030b6185876e660
williamrolando88 <william.rolando88@hotmail.com>
2022-05-02 19:47:32 -0500

    Add: Render redux stored value

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -14,11 +14,6 @@ const App = () => {
         </Routes>
       </BrowserRouter>
     </Provider>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Greet />} />
-    //   </Routes>
-    // </BrowserRouter>
   );
 };
 
--- app/javascript/components/Greet.jsx
+++ app/javascript/components/Greet.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Greet = () => {
-  return <div>Greet</div>;
+  const greeting = useSelector((store) => store.greetings.greet);
+
+  return (
+    <div>
+      <div>{greeting}</div>
+      <button>Greet me!</button>
+    </div>
+  );
 };
 
 export default Greet;
